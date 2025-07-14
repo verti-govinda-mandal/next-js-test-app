bf2f7a09ef733d0428b975a709d57f4735702f07
Govinda Mandal <verti_govinda_mandal@golfdistrict.com>
2025-07-14 18:23:54 +0530

    Modified next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config, options) {
+    if (options.isServer) config.devtool = 'source-map';
+    return config;
+  },
 };
 
 export default nextConfig;

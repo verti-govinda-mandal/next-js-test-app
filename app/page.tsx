4ce533734793eae083c98ccf372523d64f6b0082
Govinda Mandal <verti_govinda_mandal@golfdistrict.com>
2025-07-16 22:14:28 +0530

    Updated Home Component

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
     setTimeout(() => {
       throw new Error("Test error to verify GlitchTip integration");
     });
+    console.error("This is a test error to verify GlitchTip integration");
   };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
